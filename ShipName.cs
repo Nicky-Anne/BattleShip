2bba0b9856006ffe773d6d11b82c6430e1f78ba1
Nicky-Anne <48523186+Nicky-Anne@users.noreply.github.com>
2019-04-21 23:57:21 +0800

    fixed

---
+++ ShipName.cs
@@ -0,0 +1,32 @@
+/// <summary>
+/// Ship name.
+/// </summary>
+public enum ShipName
+{None = 0,
+	None = 0,
+
+	/// <summary>
+	/// Tug.
+	/// </summary>
+	Tug = 1,
+
+	/// <summary>
+	/// The submarine.
+	/// </summary>
+	Submarine = 2,
+
+	/// <summary>
+	/// The destroyer.
+	/// </summary>
+	Destroyer = 3,
+
+	/// <summary>
+	/// The battleship.
+	/// </summary>
+	Battleship = 4,
+
+	/// <summary>
+	/// Aircraft carrier.
+	/// </summary>
+	AircraftCarrier = 5
+}
