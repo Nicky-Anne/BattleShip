745fbfe5fcab4c439221588d98eb60d600e60509
Maryvkth <48543915+Maryvkth@users.noreply.github.com>
2019-04-30 13:00:30 +0800

    Update GameLogic.cs
    Change bgm name

--- src/GameLogic.cs
+++ src/GameLogic.cs
@@ -19,7 +19,7 @@ namespace GameLogic
             // Load Resources
             GameResources.LoadResources();
 
-            SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+            SwinGame.PlayMusic(GameResources.GameMusic("BGM"));
 
             // Game Loop
             while (!(true == SwinGame.WindowCloseRequested() || GameController.CurrentState == GameState.Quitting))
